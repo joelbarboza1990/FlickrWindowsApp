d02fe68e25be6fb0bb8063b733a1a156760edeef
joelbarboza <joel.barboza@ridgehead.com>
2019-05-07 01:24:03 +0530

    Checkin for changes done

---
+++ FlickrViewerApplication/FlickrForm1.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlickrViewerApplication
+{
+    partial class FlickrViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.ImageViewer = new System.Windows.Forms.ListView();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.tweetLabel = new System.Windows.Forms.Label();
+            this.tweeterGridView = new System.Windows.Forms.DataGridView();
+            this.loadingLabel = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tweeterGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLabel.Location = new System.Drawing.Point(527, 33);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Padding = new System.Windows.Forms.Padding(2);
+            this.searchLabel.Size = new System.Drawing.Size(62, 22);
+            this.searchLabel.TabIndex = 0;
+            this.searchLabel.Text = "Search";
+            // 
+            // searchBox
+            // 
+            this.searchBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(595, 31);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(536, 26);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBox_KeyUp);
+            // 
+            // ImageViewer
+            // 
+            this.ImageViewer.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImageViewer.Location = new System.Drawing.Point(5, 12);
+            this.ImageViewer.Name = "ImageViewer";
+            this.ImageViewer.Size = new System.Drawing.Size(856, 640);
+            this.ImageViewer.TabIndex = 3;
+            this.ImageViewer.UseCompatibleStateImageBehavior = false;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.tweetLabel);
+            this.mainPanel.Controls.Add(this.tweeterGridView);
+            this.mainPanel.Controls.Add(this.ImageViewer);
+            this.mainPanel.Location = new System.Drawing.Point(7, 170);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1682, 667);
+            this.mainPanel.TabIndex = 4;
+            this.mainPanel.Visible = false;
+            // 
+            // tweetLabel
+            // 
+            this.tweetLabel.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tweetLabel.Location = new System.Drawing.Point(867, 12);
+            this.tweetLabel.Name = "tweetLabel";
+            this.tweetLabel.Size = new System.Drawing.Size(801, 56);
+            this.tweetLabel.TabIndex = 5;
+            this.tweetLabel.Text = "Loading Please Wait ...";
+            this.tweetLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tweeterGridView
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.NullValue = null;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(5);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tweeterGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tweeterGridView.ColumnHeadersHeight = 100;
+            this.tweeterGridView.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tweeterGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.tweeterGridView.Location = new System.Drawing.Point(867, 85);
+            this.tweeterGridView.Name = "tweeterGridView";
+            this.tweeterGridView.RowHeadersVisible = false;
+            this.tweeterGridView.Size = new System.Drawing.Size(812, 567);
+            this.tweeterGridView.TabIndex = 4;
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadingLabel.Location = new System.Drawing.Point(9, 84);
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.Size = new System.Drawing.Size(1677, 56);
+            this.loadingLabel.TabIndex = 0;
+            this.loadingLabel.Text = "Loading Please Wait ...";
+            this.loadingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FlickrViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(1708, 849);
+            this.Controls.Add(this.loadingLabel);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.searchLabel);
+            this.Name = "FlickrViewer";
+            this.Text = "Flickr Viewer";
+            this.mainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tweeterGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ListView ImageViewer;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label loadingLabel;
+        private DataGridView tweeterGridView;
+        private Label tweetLabel;
+    }
+}
+
---
+++ FlickrViewerApplication/FlickrForm1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using TweetSharp;
+
+namespace FlickrViewerApplication
+{
+    public partial class FlickrViewer : Form
+    {
+        public FlickrViewer()
+        {
+            InitializeComponent();
+            loadingLabel.Text = "";
+            ActiveControl = searchBox;
+            searchBox.Focus();
+            loadingLabel.Show();
+        }
+
+        private void searchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (searchBox.Text.Equals(string.Empty)) return;
+            if (e.KeyCode == Keys.Enter)
+            {
+                loadingLabel.Text = "Loading Data, Please wait ...";
+                loadingLabel.Show();
+                tweetLabel.Text = "Loading Tweets Please wait...";
+                GetTweets(searchBox.Text);
+                mainPanel.Visible = true;
+                ImageViewer.Items.Clear();
+                GetSearchImages(searchBox.Text);
+                tweetLabel.Text = "Tweets for " + searchBox.Text;
+            }
+        }
+
+        private async void GetSearchImages(string value)
+        {
+            var url = $"http://api.flickr.com/services/feeds/photos_public.gne?tags={value}&tagmode=any&format=json";
+
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var fileJsonString = await response.Content.ReadAsStringAsync();
+                        var data = fileJsonString;
+                        var jsonFlickrObj = BuildJsonData(data);
+                        loadingLabel.Text = jsonFlickrObj.title;
+                        DisplayData(jsonFlickrObj);
+                    }
+                }
+            }
+        }
+
+        private static jsonFlickr BuildJsonData(string data)
+        {
+            var removedData = data.Replace("jsonFlickrFeed(", "");
+            var newString = removedData.Remove(removedData.Length - 1);
+            var jsonFlickr = JsonConvert.DeserializeObject<jsonFlickr>(newString);
+            return jsonFlickr;
+        }
+
+        private void DisplayData(jsonFlickr flickrObj)
+        {
+            if (flickrObj.items == null) return;
+            var imageList = (from obj in flickrObj.items where obj.media != null select obj.media.m).ToList();
+            var img = new ImageList
+            {
+                ImageSize = new Size(230, 200),
+                ColorDepth = ColorDepth.Depth32Bit
+            };
+            if (flickrObj.items.Count == 0)
+            {
+                loadingLabel.Text = "No Items returned for search " + searchBox.Text;
+            }
+            for (var i = 0; i < flickrObj.items.Count; i++)
+            {
+                var imageObjLink = flickrObj.items[i].media;
+                Image im;
+                try
+                {
+                    var w = new WebClient();
+                    var imageByte = w.DownloadData(imageObjLink.m);
+                    if (imageByte == null) continue;
+
+                    var stream = new MemoryStream(imageByte);
+                    im = Image.FromStream(stream);
+                }
+                catch (Exception exception)
+                {
+                    continue;
+                }
+
+                img.Images.Add(im);
+
+
+                var uri = new Uri(imageList[i]);
+                ImageViewer.Items.Add(flickrObj.items[i].title, i);
+            }
+            ImageViewer.LargeImageList = img;
+        }
+
+        private void GetTweets(string keyword)
+        {
+            var _consumerKey = "MhahGOxxlpo5eIwfvgqm8MM30";
+            var _consumerSecret = "vupTzaTaAE5SERotaQl8IhsMCZiwDIukKFmiETCsw1FkD08QPk"; // Your key  
+            var _accessToken = "357611961-bgTltJYoOC2fSGD4acF4UqUIPH7iXIiAQzmrmOMk"; // Your key  
+            var _accessTokenSecret = "AMRu5ZQLqTPJLNQLHqAYjn7qiAGSAja9WXBvmXrX9PCrD"; // Your key  
+
+            var twitterService = new TwitterService(_consumerKey, _consumerSecret);
+            twitterService.AuthenticateWith(_accessToken, _accessTokenSecret);
+
+
+            var tweetsSearch = twitterService.Search(new SearchOptions {Q = keyword, Count = 100});
+                //Resulttype = TwitterSearchResultType.Recent,
+            //Resulttype can be TwitterSearchResultType.Popular or TwitterSearchResultType.Mixed or TwitterSearchResultType.Recent  
+            var resultList = new List<TwitterStatus>(tweetsSearch.Statuses);
+            tweeterGridView.Rows.Clear();
+            tweeterGridView.Refresh();
+            tweeterGridView.ColumnCount = 1;
+            tweeterGridView.Columns[0].HeaderText = "Tweets for " + keyword;
+            foreach (var tweet in resultList)
+            {
+                tweeterGridView.Rows.Add(tweet.Author.ScreenName + " :--> " + tweet.Text + " @" + tweet.CreatedDate);
+            }
+            tweeterGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            tweeterGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
