c42361e0c7495dae5720e778709b94f1c54ea4c4
joelbarboza <joel.barboza@ridgehead.com>
2019-05-09 01:22:43 +0530

    Added unit tests

---
+++ FlickrViewerApplicationUnitTest/FlickrServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlickrViewerApplication.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlickrViewerApplicationUnitTest
+{
+    [TestClass]
+    public class FlickrServiceTest
+    {
+
+        [TestMethod]
+        public void BuildJsonData_NullParam()
+        {
+            FlickrService instance = new FlickrService();
+            var returnValue = instance.BuildFlickrJsonData("");
+            Assert.AreEqual(null,returnValue);
+        }
+
+        [TestMethod]
+        public void BuildJsonData_ValidStringParam()
+        {
+            FlickrService instance = new FlickrService();
+            var returnValue = instance.BuildFlickrJsonData(TestData.GetJsonData());
+            Assert.IsNotNull(returnValue);
+            Assert.AreEqual(20,returnValue.Items.Count);
+            Assert.IsNotNull(returnValue.Description);
+            Assert.IsNotNull(returnValue.Modified);
+        }
+    }
+}
---
+++ FlickrViewerApplicationUnitTest/TestData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlickrViewerApplicationUnitTest
+{
+    public class TestData
+    {
+        public static string GetJsonData()
+        {
+            var stream = File.OpenText(AppDomain.CurrentDomain.BaseDirectory +"\\JsonTestData.json");
+            //Read the file              
+            return stream.ReadToEnd();
+        }
+    }
+}
